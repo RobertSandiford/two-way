2c8c771cbe88e4862c676910783b59397eec6b60
RobertSandiford <mail@robertsandiford.co.uk>
2024-06-03 16:08:42 +0100

    save for 0.1.7

--- publish.cmd
+++ publish.cmd
@@ -12,8 +12,8 @@
     npm version %* 
 ) && (
     cd shared && pnpm publish --access=public
-) && (
+) & (
     cd server && pnpm publish --access=public
-) && (
+) & (
     cd client && pnpm publish --access=public
-)
+) &
